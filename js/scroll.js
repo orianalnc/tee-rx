25b488dff310c62bc829690e4e92c03800a2f5f0
Orianalnc <32578277+orianalnc@users.noreply.github.com>
2018-03-13 16:21:07 -0400

    scroll

---
+++ js/scroll.js
@@ -0,0 +1,33 @@
+(function($){
+
+    $(document).ready(
+
+        function(){
+
+            // Comprobar si estamos, al menos, 100 px por debajo de la posición top
+            // para mostrar o esconder el botón
+            $(window).scroll(function(){
+
+                if ( $(this).scrollTop() > 100 ) {
+
+                    $('.scroll-to-top').fadeIn();
+
+                } else {
+
+                    $('.scroll-to-top').fadeOut();
+
+                }
+
+            });
+
+            // al hacer click, animar el scroll hacia arriba
+            $('.scroll-to-top').click( function( e ) {
+
+                e.preventDefault();
+                $('html, body').animate( {scrollTop : 0}, 800 );
+
+            });
+
+        });
+
+})(jQuery);
