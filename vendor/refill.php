3a207b7ad358984666b4a1122aa2d25829ed5b67
Orianalnc <ocontreras@rsegrupovzla.com>
2018-03-26 10:15:10 -0400

    Formularios validados y configurados

--- vendor/refill.php
+++ vendor/refill.php
@@ -1,21 +1,19 @@
 <?php
 
 
-    $to = "info@qubitmarketing.com";
+$to = "info@qubitmarketing.com";
 
-    $first = $_POST["first"];
-    $last = $_POST["last"];
-    $email = $_POST["email"];
-    $phone = $_POST["phone"];
-    $drName = $_POST["drName"];
-    $drPhone = $_POST["drPhone"];
-    $medication = $_POST["medication"];
-    $message = $_POST["message"];
+$first = $_POST["first"];
+$last = $_POST["last"];
+$email = $_POST["email"];
+$phone = $_POST["phone"];
+$rxNumber = $_POST["rxNumber"];
+$message = $_POST["message"];
 
-    $content = "First Name: " . $first . "\nLast Name:" . $last . "\nE-mail: " .$email . "\nPhone: " . $phone . "\nDr Name: " . $drName .  "\n Dr Phone: " . $drPhone . "\nMedication : " . $medication . "\nMessage: " .$message;
+$content = "First Name: " . $first . "\nLast Name:" . $last . "\nE-mail: " .$email . "\nPhone: " . $phone .  "\nRx Numbers : " . $rxNumber . "\nMessage: " .$message;
 
-    mail($to, "Contacto desde New Prescription", $content);
-    header("location: index.html");
+mail($to, "Contacto desde Refill Prescription", $content);
+header("location: index.html");
 
 
 
--- vendor/transfer.php
+++ vendor/transfer.php
@@ -1,21 +1,23 @@
 <?php
 
 
-    $to = "info@qubitmarketing.com";
+$to = "info@qubitmarketing.com";
 
-    $first = $_POST["first"];
-    $last = $_POST["last"];
-    $email = $_POST["email"];
-    $phone = $_POST["phone"];
-    $drName = $_POST["drName"];
-    $drPhone = $_POST["drPhone"];
-    $medication = $_POST["medication"];
-    $message = $_POST["message"];
+$first = $_POST["first"];
+$last = $_POST["last"];
+$email = $_POST["email"];
+$phone = $_POST["phone"];
+$pharmacyName = $_POST["pharmacyName"];
+$pharmacyPhone = $_POST["pharmacyPhone"];
+$rxNumber = $_POST["rxNumber"];
+$drName = $_POST["drName"];
+$drPhone = $_POST["drPhone"];
+$message = $_POST["message"];
 
-    $content = "First Name: " . $first . "\nLast Name:" . $last . "\nE-mail: " .$email . "\nPhone: " . $phone . "\nDr Name: " . $drName .  "\n Dr Phone: " . $drPhone . "\nMedication : " . $medication . "\nMessage: " .$message;
+$content = "First Name: " . $first . "\nLast Name:" . $last . "\nE-mail: " .$email . "\nPhone: " . $phone . "\nPharmacy Name: " . $pharmacyName . "\nPharmacy Phone: " . $pharmacyPhone . "\nRx Numbers:" . $rxNumber . "\nDr Name: " . $drName .  "\n Dr Phone: " . $drPhone .  "\nMessage: " .$message;
 
-    mail($to, "Contacto desde New Prescription", $content);
-    header("location: index.html");
+mail($to, "Contacto desde Transfer Prescription", $content);
+header("location: index.html");
 
 
 
